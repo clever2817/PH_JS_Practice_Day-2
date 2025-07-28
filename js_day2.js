7888853d8058db214750aa16e9abf92b631a204c
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-28 22:37:37 +0600

    Problem 2 solved

--- js_day2.js
+++ js_day2.js
@@ -13,5 +13,30 @@ else
 }
 
 
-// problem 2
+// problem 2 (BMI Calculator)
 
+let weight = 70; // weight in Kilograms(kg)
+
+let height = 1.8  // height in meters(m)
+
+let BMI = Number((weight / height ** 2).toFixed(1));
+
+if(BMI < 18.5)
+{
+    console.log("You are underweight.");
+}
+
+else if(BMI >= 18.5 && BMI <= 24.9)
+{
+    console.log("You are normal.");
+}
+
+else if(BMI >= 25 && BMI <= 29.9)
+{
+    console.log("You are overweight.");
+}
+
+else
+{
+    console.log("You are obese.");
+}
