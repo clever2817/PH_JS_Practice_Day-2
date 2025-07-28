c36c30b4ef1aa767527a217f3358bdf77619c0c5
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-28 23:01:50 +0600

    Problem 5 Solved

--- js_day2.js
+++ js_day2.js
@@ -48,7 +48,7 @@ if (score >= 0 && score <= 100) {
 }
 
 
-// Problem 4
+// Problem 4 (Nested Friend)
 
 let myScore = 61;
 
@@ -81,3 +81,27 @@ else if(myScore < 80)
 {
     console.log("Go to home and sleep and act sad.");
 }
+
+
+// Problem 5 (Ternary Number)
+
+let num1 = 10;
+
+let num2 = 20;
+
+let result;
+
+if(num1 > num2)
+{
+    result = num1 * 2;
+}
+
+else
+{
+    result = num1 + num2;
+}
+
+
+(num1 > num2) ? result = num1 * 2 : result = num1 + num2;
+
+console.log(result);
