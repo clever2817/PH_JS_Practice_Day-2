798eed994bf93c84f85524eac3b48baac58c8e14
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-28 22:21:48 +0600

    Problem 1 solved

---
+++ js_day2.js
@@ -0,0 +1,17 @@
+// Problem 1 (Free Drinks)
+
+let burger = 350;
+
+if(burger > 500)
+{
+    console.log("You will get free coke.");
+}
+
+else
+{
+    console.log("You have to pay 30tk for coke.");
+}
+
+
+// problem 2
+
