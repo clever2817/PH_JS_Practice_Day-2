bd38d0c29bafc88bae7ff50ad5fbc7f4e5c99a97
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-28 22:43:36 +0600

    Problem 3 solved

--- js_day2.js
+++ js_day2.js
@@ -2,41 +2,47 @@
 
 let burger = 350;
 
-if(burger > 500)
-{
-    console.log("You will get free coke.");
+if (burger > 500) {
+  console.log("You will get free coke.");
+} else {
+  console.log("You have to pay 30tk for coke.");
 }
-
-else
-{
-    console.log("You have to pay 30tk for coke.");
-}
-
 
 // problem 2 (BMI Calculator)
 
 let weight = 70; // weight in Kilograms(kg)
 
-let height = 1.8  // height in meters(m)
+let height = 1.8; // height in meters(m)
 
 let BMI = Number((weight / height ** 2).toFixed(1));
 
-if(BMI < 18.5)
-{
-    console.log("You are underweight.");
+if (BMI < 18.5) {
+  console.log("You are underweight.");
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+  console.log("You are normal.");
+} else if (BMI >= 25 && BMI <= 29.9) {
+  console.log("You are overweight.");
+} else {
+  console.log("You are obese.");
 }
 
-else if(BMI >= 18.5 && BMI <= 24.9)
-{
-    console.log("You are normal.");
-}
+// Problem 3 (Grade Calculator)
 
-else if(BMI >= 25 && BMI <= 29.9)
-{
-    console.log("You are overweight.");
-}
+let score = 69;
 
-else
-{
-    console.log("You are obese.");
+if (score >= 0 && score <= 100) {
+  if (score >= 90 && score <= 100) {
+    console.log("A");
+  } else if (score >= 80 && score <= 89) {
+    console.log("B");
+  } else if (score >= 70 && score <= 79) {
+    console.log("C");
+  } else if (score >= 60 && score <= 69) {
+    console.log("D");
+  }
+
+  else 
+  {
+    console.log("F");
+  }
 }
